0e0890a3003ebf97739bec93a8d750f6f315ad1f
Luciano Puig <luciano.puig95@gmail.com>
2018-05-29 15:21:26 -0300

    continuacion del vVerViajes

--- application/models/mViajes.php
+++ application/models/mViajes.php
@@ -1,4 +1,4 @@
-<?php 
+	<?php 
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 /**
@@ -9,25 +9,13 @@ class MViajes extends CI_Model{
 	function __construct()
 	{
 		parent::__construct();
+		$this->load->model('mLogin');
 	}
-	/*public function get_Viajes(){
-		$this->db->select('*');
-		$this->db->from('viaje');
-		$this->db->join('usuario','usuario.idUsuario=viaje.usuarioId', 'inner');
-		$this->db->join('vehiculo','vehiculo.idVehiculo=viaje.vehiculoId', 'inner');
-		$this->db->limit(10);
-		$query =$this->db->get();
-		$resultado= $query->result();
-		return $resultado;
-	}*/
-	/*public function get_mis_viajes($usuarioId){
-		$this->db->where('usuarioId', $usuarioId);
-		$query = $this->db->get('viaje');
-		return $query->result_array();
-	}*/
+
 	public function get_viajes($usuarioId='Nulo'){
 		if ($usuarioId == 'Nulo') {
 			$query = $this->db->get('viaje');
+			
 		} else {
 			$this->db->where('usuarioId', $usuarioId);
 			$query = $this->db->get('viaje');
--- application/views/loguedIn/vVerViajes.php
+++ application/views/loguedIn/vVerViajes.php
@@ -1,34 +1,34 @@
 
 
      <section id=vVerViajes class="content-section text-center">
-        <div class="col-md-8 mb-4">
+        <div class="col-form-label-sm">
           <div class="card h-99">
             <h6 class="stroke"><font color="salmon"><u><?php echo $titulo ?></u></font></h6>
-            <table class="table table-bordered">
+            <table class="table user table-hover" style="background-color:black; border: 1px solid salmon; ">
               <thead>
                 <tr>
-                  <th scope="col" valign="center" align="center"><font color="black">Origen</font></th>
-                  <th scope="col" valign="center" align="center"><font color="black">Destino</font></th>
-                  <th scope="col" valign="center" align="center"><font color="black">Fecha</font></th>
-                  <th scope="col" valign="center" align="center"><font color="black">Hora</font></th>
-                  <th scope="col" valign="center" align="center"><font color="black">Asientos</font></th>
-                  <th scope="col" valign="center" align="center"><font color="black">Costo</font></th>
-                  <th scope="col" valign="center" align="center"><font color="black">Periodico</font></th>
-                  <th scope="col" valign="center" align="center"><font color="black">Vehiculo</font></th>
-                  <th scope="col" valign="center" align="center"><font color="black">Usuario</font></th>
+                  <th scope="col" valign="center" align="center"><font color="salmon">Origen</font></th>
+                  <th scope="col" valign="center" align="center"><font color="salmon">Destino</font></th>
+                  <th scope="col" valign="center" align="center"><font color="salmon">Fecha</font></th>
+                  <th scope="col" valign="center" align="center"><font color="salmon">Hora</font></th>
+                  <th scope="col" valign="center" align="center"><font color="salmon">Asientos</font></th>
+                  <th scope="col" valign="center" align="center"><font color="salmon">Costo</font></th>
+                  <th scope="col" valign="center" align="center"><font color="salmon">Periodico</font></th>
+                  <th scope="col" valign="center" align="center"><font color="salmon">Vehiculo</font></th>
+                  <th scope="col" valign="center" align="center"><font color="salmon">Usuario</font></th>
                 </tr>
               </thead>
                 <?php  foreach ($viajes as $row) {  ?>
                 <tr>
-                  <td valign="center" align="center"><font color="black"><?php echo $row['origen'] ?></font></td>
-                  <td valign="center" align="center"><font color="black"><?php echo $row['destino'] ?></font></td>
-                  <td valign="center" align="center"><font color="black"><?php echo $row['fecha'] ?></font></td>
-                  <td valign="center" align="center"><font color="black"><?php echo $row['hora'] ?> hs.</font></td>
-                  <td valign="center" align="center"><font color="black"><?php echo $row['asientosDisp'] ?> </td>
-                  <td valign="center" align="center"><font color="black"><?php echo $row['Costo'] ?></font></td>
-                  <td valign="center" align="center"><font color="black"><?php if($row['Periodico']==1){echo "Sí";} else {echo "No";}?></font></td>
-                  <td valign="center" align="center"valign="center" align="center"><font color="black"><?php echo $row['vehiculoId'] ?></font></td>
-                  <td valign="center" align="center"valign="center" align="center"><font color="black"><?php echo $row['usuarioId'] ?></font></td>
+                  <td valign="center" align="center"><font color="salmon"><?php echo $row['origen'] ?></font></td>
+                  <td valign="center" align="center"><font color="salmon"><?php echo $row['destino'] ?></font></td>
+                  <td valign="center" align="center"><font color="salmon"><?php echo $row['fecha'] ?></font></td>
+                  <td valign="center" align="center"><font color="salmon"><?php echo $row['hora'] ?> hs.</font></td>
+                  <td valign="center" align="center"><font color="salmon"><?php echo $row['asientosDisp'] ?> </td>
+                  <td valign="center" align="center"><font color="salmon"><?php echo $row['Costo'] ?></font></td>
+                  <td valign="center" align="center"><font color="salmon"><?php if($row['Periodico']==1){echo "Sí";} else {echo "No";}?></font></td>
+                  <td valign="center" align="center"valign="center" align="center"><font color="salmon"><?php echo $row['vehiculoId'] ?></font></td>
+                  <td valign="center" align="center"valign="center" align="center"><font color="salmon"><?php echo $row['usuarioId'] ?></font></td>
                 </tr>
               <?php }   ?>
               </tbody>
